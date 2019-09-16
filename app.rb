9564b9cb50bf7967bfea961640fffea9d295f7f2
kel29 <krista.loven.87@gmail.com>
2019-09-16 10:33:53 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get('/name') do
+        "My name is Krista"
+    end
+
+    get('/hometown') do
+        "My hometown is Brainerd, MN"
+    end
+
+    get('/favorite-song') do
+        "My favorite song is The Chicken Dance"
+    end
+
 end
